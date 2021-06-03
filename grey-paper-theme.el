a57f64723003ecf7d3c8ad29d82cfb2558792b91
Kang-min Liu <gugod@gugod.org>
2021-06-04 08:46:19 +0900

    give those colors meaningful names

--- grey-paper-theme.el
+++ grey-paper-theme.el
@@ -15,24 +15,36 @@
 (deftheme grey-paper
   "Grayscale like eink")
 
-(custom-theme-set-faces
- 'grey-paper
- '(link ((t (:foreground "#224022" :underline t))))
- '(cperl-array-face ((t (:background "#d0d1d0" :foreground "#222a33" :bold t))))
- '(cperl-hash-face ((t (:background "#d0d1d0" :foreground "#222a33" :bold t))))
- '(cperl-nonoverridable-face ((t (:background "#d0d1d0" :foreground "#222a33" :bold t))))
- '(default ((t (:background "#d0d1d0" :foreground "#222a33"))))
- '(font-lock-builtin-face ((t (:foreground "#222a33"))))
- '(font-lock-comment-face ((t (:foreground "#909090"))))
- '(font-lock-constant-face ((t (:foreground "#111111"))))
- '(font-lock-function-name-face ((t (:foreground "#222a33" :bold t))))
- '(font-lock-keyword-face ((t (:foreground "#333a33"))))
- '(font-lock-string-face ((t (:foreground "#222a33"))))
- '(font-lock-type-face ((t (:foreground "#111a22"))))
- '(font-lock-variable-name-face ((t (:foreground "#222a33"))))
- '(fringe ((t (:background "#d0d1d0" :foreground "black"))))
- '(region ((t (:background "#c0c0c0" :distant-foreground "ns_selection_fg_color"))))
- )
+(let (
+      (bg "#d0d1d0")
+      (bg-dark "#c0c0c0")
+
+      (fg "#222a33")
+      (fg-dark "#111a22")
+      (fg-light "#909090")
+      (fg-alt "#224022")
+      )
+
+  (custom-theme-set-faces
+   'grey-paper
+   '(default ((t (:background bg :foreground fg))))
+   '(fringe ((t (:background bg :foreground fg))))
+   '(region ((t (:background bg-dark :distant-foreground "ns_selection_fg_color"))))
+   '(link ((t (:foreground fg-alt :underline t))))
+
+   '(font-lock-builtin-face ((t (:foreground fg))))
+   '(font-lock-comment-face ((t (:foreground fg-light))))
+   '(font-lock-constant-face ((t (:foreground fg-dark))))
+   '(font-lock-function-name-face ((t (:foreground fg :bold t))))
+   '(font-lock-keyword-face ((t (:foreground fg))))
+   '(font-lock-string-face ((t (:foreground fg))))
+   '(font-lock-type-face ((t (:foreground fg-dark))))
+   '(font-lock-variable-name-face ((t (:foreground fg))))
+
+   '(cperl-array-face ((t (:background bg :foreground fg :bold t))))
+   '(cperl-hash-face ((t (:background bg :foreground fg :bold t))))
+   '(cperl-nonoverridable-face ((t (:background bg :foreground fg :bold t))))
+   ))
 
 (provide-theme 'grey-paper)
 ;;; grey-paper-theme.el ends here
