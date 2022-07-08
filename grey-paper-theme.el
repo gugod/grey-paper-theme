ba4d36695d229efb9bb72e4c2b83fec7c98f9d3d
Kang-min Liu <gugod@gugod.org>
2022-07-08 09:45:09 +0900

    a few packging style fixes, according to package-lint

--- grey-paper-theme.el
+++ grey-paper-theme.el
@@ -1,4 +1,4 @@
-;;; grey-paper-theme.el --- A greyscale theme with look-n-feel of an eink display.
+;;; grey-paper-theme.el --- A greyscale theme with look-n-feel of an eink display
 ;;; -*- lexical-binding: t; -*-
 
 ;;; Author: Kang-min Liu <gugod@gugod.org>
@@ -17,15 +17,12 @@
 (deftheme grey-paper
   "A greyscale theme with look-n-feel of an eink display.")
 
-(let (
-      (bg "#d0d1d0")
+(let ((bg "#d0d1d0")
       (bg-dark "#c0c0c0")
-
       (fg "#222a33")
       (fg-dark "#111a22")
       (fg-light "#909090")
-      (fg-alt "#224022")
-      )
+      (fg-alt "#224022"))
 
   (custom-theme-set-faces
    'grey-paper
@@ -45,8 +42,7 @@
 
    `(cperl-array-face ((t (:background ,bg :foreground ,fg :bold t))))
    `(cperl-hash-face ((t (:background ,bg :foreground ,fg :bold t))))
-   `(cperl-nonoverridable-face ((t (:background ,bg :foreground ,fg :bold t))))
-   ))
+   `(cperl-nonoverridable-face ((t (:background ,bg :foreground ,fg :bold t))))))
 
 (provide-theme 'grey-paper)
 (provide 'grey-paper-theme)
